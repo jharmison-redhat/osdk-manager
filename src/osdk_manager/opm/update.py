c2cc77198df4ca50c14496fc37a9644ba50022dc
James Harmison <jharmison@redhat.com>
2020-10-02 13:11:02 -0400

    Scaffolded OPM updates

---
+++ src/osdk_manager/opm/update.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-2-Clause
+"""osdk-manager opm-specific functionality.
+
+Manager Operator SDK binary installation, and help to scaffold, release, and
+version Operator SDK-based Kubernetes operators.
+
+This subpackage includes necessary modules for updating the installation of the
+Operator Package Manager, opm.
+"""
---
+++ tests/test_opm_update.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-2-Clause
+"""osdk-manager opm update tests.
+
+Manager Operator SDK binary installation, and help to scaffold, release, and
+version Operator SDK-based Kubernetes operators.
+
+This test set validates that an update correctly installs and validates the
+latest version of opm, the Operator Package Manager, but can also be used to
+pin a version.
+"""
