c20d58d9f2b69c1b7c844c5826fc49660621a11a
James Harmison <jharmison@redhat.com>
2020-10-02 15:02:30 -0400

    Add hard check for linux platform

--- src/osdk_manager/__init__.py
+++ src/osdk_manager/__init__.py
@@ -4,3 +4,8 @@
 Manager Operator SDK binary installation, and help to scaffold, release, and
 version Operator SDK-based Kubernetes operators.
 """
+
+import platform
+
+if platform.system() != "Linux":
+    raise EnvironmentError("osdk_manager is designed only for Linux.")
