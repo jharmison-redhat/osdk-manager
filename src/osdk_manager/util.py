a054074c9af5447e58030aedf026e27b3311850b
James Harmison <jharmison@redhat.com>
2020-10-05 09:48:32 -0400

    Added shell helper

--- src/osdk_manager/util.py
+++ src/osdk_manager/util.py
@@ -7,9 +7,13 @@ version Operator SDK-based Kubernetes operators.
 This file contains utilities utilized throughout the package and modules.
 """
 
-import os
 import logging
 import logging.handlers
+import os
+import subprocess
+import shlex
+
+from typing import List, Iterable
 
 
 def get_logger(verbosity: int = None):
@@ -40,3 +44,34 @@ def get_logger(verbosity: int = None):
             stderr.setLevel(40 - (min(3, verbosity) * 10))
 
     return logger
+
+
+def _utf8ify(line_bytes: List[bytes] = None) -> str:
+    """
+    Decodes type line_bytes input as utf-8 and strips excess whitespace from
+    the end.
+    """
+    return line_bytes.decode("utf-8").rstrip()
+
+
+def shell(cmd: str = None, fail: bool = True) -> Iterable[str]:
+    """
+    Runs a command in a subprocess, yielding lines of output from it and
+    optionally failing with its non-zero return code.
+    """
+    logger = get_logger()
+    logger.debug("Running: {}".format(cmd))
+    proc = subprocess.Popen(shlex.split(cmd),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+
+    for line in map(_utf8ify, iter(proc.stdout.readline, b'')):
+        logger.debug("Line:    {}".format(line))
+        yield line
+
+    ret = proc.wait()
+    if fail and ret != 0:
+        logger.error("Command errored: {}".format(cmd))
+        exit(ret)
+    elif ret != 0:
+        logger.warning("Command returned {}: {}".format(ret, cmd))
