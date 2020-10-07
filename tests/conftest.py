380305a239a91cbd0cb22bfaab7bd6ac3a614a98
James Harmison <jharmison@redhat.com>
2020-10-07 16:40:20 -0400

    Get us to 100% on a host (still breaking in container)

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,27 @@ version Operator SDK-based Kubernetes operators.
 This file contains common fixtures used by tests for osdk-manager.
 """
 
+import os
 import pytest
+import shutil
+import tempfile
+import yaml
+
+
+settings_1 = {
+    "domain": "io",
+    "group": "operators",
+    "kinds": [
+        "PytestResource"
+    ],
+    "api_version": "v1alpha1",
+    "image": "pytest-operator",
+    "version": "0.0.1",
+    "channels": [
+        "alpha"
+    ],
+    "default_sample": "operators_v1alpha1_pytestresource.yaml",
+}
 
 
 @pytest.fixture()
@@ -31,29 +51,29 @@ def installed_osdk(request):
 @pytest.fixture()
 def new_folder():
     """Create a new temp folder, cleaning it up after the test."""
-    import shutil
-    import tempfile
-
     folder = tempfile.mkdtemp()
     yield folder
     shutil.rmtree(folder)
 
 
+def operator_settings_file(settings: dict = {}) -> str:
+    """Yield the path to a file with settings saved as YAML."""
+    operator_file = tempfile.mkstemp()[1]
+    with open(operator_file, "w") as f:
+        yaml.safe_dump(settings, f)
+    return operator_file
+
+
 @pytest.fixture()
 def operator_settings_1():
     """Return a dictionary of some basic operator settings."""
-    operator = {
-        "domain": "io",
-        "group": "operators",
-        "kinds": [
-            "PytestResource"
-        ],
-        "api_version": "v1alpha1",
-        "image": "pytest-operator",
-        "version": "0.0.1",
-        "channels": [
-            "alpha"
-        ],
-        "default_sample": "operators_v1alpha1_pytestresource.yaml",
-    }
-    return operator
+    return settings_1
+
+
+@pytest.fixture()
+def operator_settings_file_1():
+    """Yield the path to a file with operator_settings_1 saved in it."""
+    settings = {k.replace('_', '-'): v for k, v in settings_1.items()}
+    operator_file = operator_settings_file(settings)
+    yield operator_file
+    os.remove(operator_file)
